6d633d391ef0589c02403bf3d2a47c78f200558e
Kene Emenike <hunteryomom@gmail.com>
2025-01-10 19:45:18 +0100

    (Week 2)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!();
+
+	println!("hello ");//no too reason am
+
+	println!("format {} arguments","some" );
+	
+	println!("My name {}, I am the class rep of stream 1", "Dagogo William Jumbo" );
+}
